c70bb89070ac99ca2e4dac2ae809093fd1ecb88c
Jakub Pojsl <pojsl.jakub@gmail.com>
2019-11-23 15:42:59 +0100

    Precedential table created
    Legit check needed

---
+++ precedential_analysis.h
@@ -0,0 +1,17 @@
+/**
+ *      Project: IFJ – Implementace překladače imperativního jazyka IFJ19
+ *      Team 65, variant 1
+ *
+ *      Authors:
+ *          Diviš Jan	    xdivis12
+ *          Kopáček Jiří	xkopac06
+ *          Pojsl Jakub	    xpojsl00
+ *          Sasín Jonáš	    xsasin05
+ *
+ *      File: precedential_analysis.h
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "parser.h"
